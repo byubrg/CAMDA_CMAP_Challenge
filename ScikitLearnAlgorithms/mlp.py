b10a154a985a7a7974a478cf0e35a338019a4a68
Michael Scott Bradshaw <mbrad94@m8int01.fsl.byu.edu>
2018-04-12 01:05:36 -0600

    created wrapper function, gradient boosting and MLP

--- ScikitLearnAlgorithms/mlp.py
+++ ScikitLearnAlgorithms/mlp.py
@@ -1,7 +1,7 @@
 from sklearn.neural_network import MLPClassifier
 
-def mlp(X_train, X_test, y_train) :
-    MLP = MLPClassifier(hidden_layer_sizes=(30,30,30)) 
+def mlp(X_train, X_test, y_train, nodes=(30,30,30), rand='not_an_int') :
+    MLP = MLPClassifier(hidden_layer_sizes=(30,30,30,30,30,30,30,30,30,30), learning_rate_init = .0376) 
 
     ## Place the training data in the MLP to train your algorithm
     MLP.fit(X_train,y_train)
